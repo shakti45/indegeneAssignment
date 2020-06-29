12ba9809308f9f69979de0b82d7ac733f3dc3d70
shakti45 <shakti45@gmail.com>
2020-06-29 19:06:12 +0530

    setting up env and vars. Also changing directory of vars

---
+++ src/vars.js
@@ -0,0 +1,12 @@
+
+let node_env = process.NODE_ENV
+let port = process.env.PORT
+let database_url = process.env.DB_URL
+let database = process.env.DB
+
+module.exports = {
+    env : node_env, 
+    port : port ,
+    dbURL : database_url,
+    database : database 
+}
